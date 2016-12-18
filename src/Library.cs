5b23f852a72c03eec9117cf7fc49633eb17ab810
Zakaria Ra <zakaria.ra@gmail.com>
2016-12-18 22:59:27 +0100

    Done

--- src/Library.cs
+++ src/Library.cs
@@ -1,11 +1,32 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Collections;
 
-namespace ClassLibrary
+namespace FizzBuzzLibrary
 {
-    public class Class1
+    public class FizzBuzz
     {
-        public void Method1()
-        {    
+        public string Print(int start, int? end = null)
+        {
+            end = end == null ? start : end;
+            
+            IEnumerable<int> range = Enumerable.Range(start, (int)end - start + 1);   
+
+            return string.Join(" ", range.Select(x => Translate(x)).ToArray());
+        }
+
+        private string Translate(int x){
+
+            string result = null;
+
+            if(x % 3 == 0) 
+                result += "Fizz";
+
+            if(x % 5 == 0) 
+                result += "Buzz";
+
+            return result != null ? result : x.ToString();
         }
     }
 }
--- test/Tests.cs
+++ test/Tests.cs
@@ -1,14 +1,50 @@
 ﻿using System;
 using Xunit;
+using FizzBuzzLibrary; 
 
 namespace Tests
 {
     public class Tests
     {
-        [Fact]
-        public void Test1() 
+
+        FizzBuzz fizzBuzz;
+
+
+        public Tests()
         {
-            Assert.True(true);
+            fizzBuzz = new FizzBuzz();
+        }
+
+        [Fact]
+        public void test_other_numbers() 
+        {
+            var result = fizzBuzz.Print(23);
+
+            Assert.Equal("23", result);
+        }
+
+        [Fact]
+        public void  Print_Fizz_instead_of_number_which_is_divisible_by_3()
+        {
+            var result = fizzBuzz.Print(3);
+
+            Assert.Equal("Fizz", result);        
+        }
+
+        [Fact]
+        public void  Print_Buzz_instead_of_number_which_is_divisible_by_5()
+        {
+            var result = fizzBuzz.Print(5);
+
+            Assert.Equal("Buzz", result);        
+        }
+
+        [Fact]
+        public void  Print_FizzBuzz_instead_of_number_which_is_divisible_by_5_and_3()
+        {
+            var result = fizzBuzz.Print(15);
+
+            Assert.Equal("FizzBuzz", result);        
         }
     }
 }
